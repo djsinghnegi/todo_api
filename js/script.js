4318a6f47c9af40a71ce23fe8c36cf322e813465
Dhananjay Negi <dhananjaynegi@Dhananjays-MacBook-Pro.local>
2019-07-04 17:24:54 +0530

    more

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(
+    ()=>{
+        let newtask = $('#newtask')
+        let tasklist = $('#list')
+        let addtask = $('#addtask')
+        function reloadList(data){
+            tasklist.empty()
+            if(data.length==0)
+                return data.forEach( (task) => tasklist.append( $('<li>').text(task) ) )
+        }
+        $.get( '/tasks', function(data) { reloadList(data) } )
+        addtask.click( 
+            function() { 
+                $.post( '/tasks', {task:newtask.val()}, (data)=>{reloadList(data), newtask.val('')} )
+            }
+        )
+    }
+)
